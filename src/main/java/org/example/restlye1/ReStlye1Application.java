8c176466855ad0054aa6fa663f91f760f4def6f8
raiuap <rayan.oujjahannou@gmail.com>
2025-07-24 21:06:59 +0200

    First message

---
+++ src/main/java/org/example/restlye1/ReStlye1Application.java
@@ -0,0 +1,13 @@
+package org.example.restlye1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReStlye1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReStlye1Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/restlye1/ReStlye1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.restlye1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReStlye1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
